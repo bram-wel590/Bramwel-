3dc58b5f587c38318f74aefcbfe2a7bf59dd01c8
bram-wel590 <bramwelimberi@gmail.com>
2026-03-26 14:03:32 +0300

    Create Seat.java entity class

---
+++ src/main/java/com/kenyabusbooking/entity/Seat.java
@@ -0,0 +1,49 @@
+package com.kenyabusbooking.entity;
+
+public class Seat {
+    private Long seatId;
+    private Long busId;
+    private String seatNumber;
+    private boolean isAvailable;
+    private String seatType;
+
+    public Long getSeatId() {
+        return seatId;
+    }
+
+    public void setSeatId(Long seatId) {
+        this.seatId = seatId;
+    }
+
+    public Long getBusId() {
+        return busId;
+    }
+
+    public void setBusId(Long busId) {
+        this.busId = busId;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public String getSeatType() {
+        return seatType;
+    }
+
+    public void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+}
